5049ec5f9fafd239e22d5201e2b218b0c82d6fb9
turhanb <turhanb@gmail.com>
2017-11-05 17:54:05 +0000

    Init CS1702_A1

---
+++ src/uk/ac/brunel/cs1702/GradeCalculator.java
@@ -0,0 +1,14 @@
+package uk.ac.brunel.cs1702;
+
+public class GradeCalculator {
+
+	//Declare all required Fields here as explained in the worksheet A1. 
+	
+	public static void main(String[] args) {
+	//Implement your program logic here to process the Fields declared above.
+	//Feel free to declare additional local variables in the main method.
+	//Feel free to create other methods and call them from the main method.
+	//Your program will be assessed based on its output as explained in the worksheet A1.	
+		
+	}
+}
